6496bdbf53e6684ab842dbee5c66264685e73a84
akhilesh <akhilesh00k1@gmail.com>
2021-05-23 21:59:20 +0530

    last update

--- bubblesort.java
+++ bubblesort.java
@@ -22,11 +22,11 @@ public class bubblesort {
         
     }
     public static void sort(int[]arr) {
-        for (int k = 0; k < arr.length-1; k++) {
+        for (int pass = 0; pass < arr.length-1; pass++) {
             int flag=0;
-            for (int i = 0; i < arr.length-1-k; i++) {
-                if(arr[i]>arr[i+1]){
-                    swap(arr, i, i+1);
+            for (int current = 0; current < arr.length-1-pass; current++) {
+                if(arr[current]>arr[current+1]){
+                    swap(arr, current, current+1);
                     flag=1;
                     
                 }
