8250bf5fc725ffb409ef24be18e164cec00549c8
akhilesh <akhilesh00k1@gmail.com>
2021-06-06 18:48:25 +0530

    correction

--- twodarray.java
+++ twodarray.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class twodarray{
     public static void main(String[] args) {
         int[][]array=takein();
+        display(array);
     
 
+        
+    }
+    public static void display(int[][] arr) {
+        for (int row=0;row<=arr.length-1;row++){
+            for(int col=0;col<=arr[row].length-1;col++){
+                System.out.print(arr[row][col]+" ");
+
+            }
+            System.out.println("");
+        }
         
     }
     
@@ -15,11 +26,11 @@ public class twodarray{
         int rows=scan.nextInt();
         
         arr=new int[rows][];
-        for(int row=0;row<=rows;row++){
+        for(int row=0;row<=rows-1;row++){
             System.out.println("enter the no of cols in row"+row);
             int cols=scan.nextInt();
             arr[row]=new int[cols];
-            for (int col=0;col<=cols;col++){
+            for (int col=0;col<=cols-1;col++){
                 System.out.println("enter the valuer for row"+row+"column"+col);
                 arr[row][col]=scan.nextInt();
 
